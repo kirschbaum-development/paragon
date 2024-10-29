d8e021edda65208cb2911e4efa5bab004f7d6c65
Luís Dalmolin <luis.nh@gmail.com>
2024-10-29 12:18:48 -0300

    Not using a custom method from L11

--- src/Commands/GenerateEnumsCommand.php
+++ src/Commands/GenerateEnumsCommand.php
@@ -47,7 +47,7 @@ class GenerateEnumsCommand extends Command
      */
     protected function enums(): Collection
     {
-        return DiscoverEnums::within(app_path(config()->string('paragon.enums.paths.php')))
+        return DiscoverEnums::within(app_path((string) config('paragon.enums.paths.php')))
             ->reject(function ($enum) {
                 if (! enum_exists($enum)) {
                     return true;
@@ -55,13 +55,13 @@ class GenerateEnumsCommand extends Command
 
                 $reflector = new ReflectionEnum($enum);
 
-                $paths = Arr::map(
+                $pathsToIgnore = Arr::map(
                     Arr::wrap(config('paragon.enums.paths.ignore')),
                     fn (string $path): string => Str::finish(app_path($path), '/'),
                 );
 
                 return $reflector->getAttributes(IgnoreParagon::class)
-                    || Str::startsWith((string) $reflector->getFileName(), $paths);
+                    || Str::startsWith((string) $reflector->getFileName(), $pathsToIgnore);
             })
             ->values();
     }
